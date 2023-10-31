3f2b64e70dc1626c32c11e48b23e70c064d6c099
Dawid Jasiński <ambas90@gmail.com>
2023-10-31 14:07:31 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -8,9 +8,13 @@ const ingredients = [
 ];
 
 const list = document.querySelector("#ingredients");
+const ingredientsList = [];
 
-const markup = ingredients
-  .map((ingredient) => `<li class="item">${ingredient}</li>`)
-  .join("");
+for (const element of ingredients) {
+  const ingredient = document.createElement("li");
+  ingredient.textContent = element;
+  ingredient.classList.add("item");
+  ingredientsList.push(ingredient);
+}
 
-list.insertAdjacentHTML("beforeend", markup);
+list.append(...ingredientsList);
