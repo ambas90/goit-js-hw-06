823b46b568c1d33abdc48841e27b8adf178b1e51
Dawid Jasiński <ambas90@gmail.com>
2023-11-01 21:58:32 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const colorText = document.querySelector(".color");
+
+btn.addEventListener("click", bgColorChange);
+
+function bgColorChange() {
+  let bgColor = getRandomHexColor();
+  body.style.backgroundColor = bgColor;
+  colorText.textContent = bgColor;
+}
