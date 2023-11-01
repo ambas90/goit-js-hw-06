a66336facf7db2b71218359b2bd72d88f6d9657c
Dawid Jasiński <ambas90@gmail.com>
2023-11-01 21:23:36 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Wszystkie pola muszą zostać wypełnione!");
+  } else {
+    const user = {
+      email,
+      password,
+    };
+
+    console.log(user);
+
+    form.reset();
+  }
+}
