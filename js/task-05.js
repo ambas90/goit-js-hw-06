b3494fb441c2d7ae378f45f9b23c9cf998088d65
Dawid Jasiński <ambas90@gmail.com>
2023-11-01 10:20:41 +0100

    task 5 fix

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const textInput = document.querySelector("#name-input");
+const textOutput = document.querySelector("#name-output");
+
+textInput.addEventListener("input", (event) => {
+  if (textInput.value.replace(/ /g, "").length === 0) {
+    textOutput.textContent = "Anonymous";
+  } else {
+    textOutput.textContent = event.currentTarget.value;
+  }
+  console.log(textInput.value);
+});
